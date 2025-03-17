3d588432dd20cbdbb5ce6a66be156f9019d17894
Rafał Łomotowski <rafal.lomotowski@gmail.com>
2025-03-17 18:33:05 +0100

    Usunięcie /english-dream z base vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/english-dream",
+  base: "/",
   plugins: [
     tailwindcss(),
 	react()
